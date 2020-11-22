c5f5cdc805506035468bf473ad3eb360baac1b73
Guilherme Vinicius Barbosa <guilherme.barbosa@fri.to>
2020-11-22 16:46:35 -0300

    Exibindo apenas os posts do usuario logado

--- app/src/main/java/com/example/tsisocialapp/utils/Util.kt
+++ app/src/main/java/com/example/tsisocialapp/utils/Util.kt
@@ -2,15 +2,17 @@ package com.example.tsisocialapp.utils
 
 import android.app.AlertDialog
 import android.content.Context
+import com.example.tsisocialapp.model.Post
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.database.DataSnapshot
 
-    fun getCurrentUser(): FirebaseUser? {
+fun getCurrentUser(): FirebaseUser? {
         val auth = FirebaseAuth.getInstance()
         return auth.currentUser
     }
 
-    fun alert (title: String, msg: String, Context: Context) {
+fun alert (title: String, msg: String, Context: Context) {
         val builder = AlertDialog.Builder(Context)
 
         builder
@@ -20,3 +22,26 @@ import com.google.firebase.auth.FirebaseUser
             .create()
             .show()
     }
+
+fun convertSnapshotToPostList(snapshot: DataSnapshot): List<Post>{
+    val postsList = arrayListOf<Post>()
+
+    snapshot.children.forEach {
+        val mapa = it.getValue() as HashMap<String, Any>
+
+        val id = mapa.get("id") as String
+        val user_uid = mapa.get("user_uid") as String
+        val user = mapa.get("user") as String
+        val timestamp = mapa.get("timestamp") as String
+        val title = mapa.get("title") as String
+        val text = mapa.get("text") as String
+        val category = mapa.get("category") as String
+        val likes = mapa.get("likes") as Long
+        val image = mapa.get("image") as String
+
+        val post = Post(id, user_uid, user, timestamp, title, text, category, likes, image)
+        postsList.add(post)
+    }
+
+    return postsList
+}
--- app/src/main/java/com/example/tsisocialapp/views/activities/PostListActivity.kt
+++ app/src/main/java/com/example/tsisocialapp/views/activities/PostListActivity.kt
@@ -6,6 +6,8 @@ import android.util.Log
 import android.widget.Toast
 import com.example.tsisocialapp.R
 import com.example.tsisocialapp.model.Post
+import com.example.tsisocialapp.utils.convertSnapshotToPostList
+import com.example.tsisocialapp.utils.getCurrentUser
 import com.google.firebase.database.*
 import kotlinx.android.synthetic.main.activity_post_list.*
 import kotlinx.android.synthetic.main.options_card.*
@@ -37,37 +39,18 @@ class PostListActivity : AppCompatActivity() {
         database?.addValueEventListener(vEvListener)
     }
 
-    fun convertSnapshotToPostList(snapshot: DataSnapshot): List<Post>{
-        val postsList = arrayListOf<Post>()
-
-        snapshot.children.forEach {
-            val mapa = it.getValue() as HashMap<String, Any>
-
-            val id = mapa.get("id") as String
-            val user_uid = mapa.get("user_uid") as String
-            val user = mapa.get("user") as String
-            val timestamp = mapa.get("timestamp") as String
-            val title = mapa.get("title") as String
-            val text = mapa.get("text") as String
-            val category = mapa.get("category") as String
-            val likes = mapa.get("likes") as Long
-            val image = mapa.get("image") as String
-
-            val post = Post(id, user_uid, user, timestamp, title, text, category, likes, image)
-            postsList.add(post)
-        }
-
-        return postsList
-    }
-
     fun refreshUI(posts: List<Post>){
         container.removeAllViews()
+
+        val uid = getCurrentUser()!!.uid
 
         posts.forEach {
             val cardPost = layoutInflater.inflate(R.layout.options_card, container, false)
 
-            cardPost.txtBtn.text = it.title
-            container.addView(cardPost)
+            if (it.user_uid == uid){
+                cardPost.txtBtn.text = it.title
+                container.addView(cardPost)
+            }
         }
     }
 }
