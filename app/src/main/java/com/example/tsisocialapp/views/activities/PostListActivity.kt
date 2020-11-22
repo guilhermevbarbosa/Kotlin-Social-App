529b50378f08b1fe16f695e37616c32c3da7b75d
Guilherme Vinicius Barbosa <guilherme.barbosa@fri.to>
2020-11-22 13:37:32 -0300

    Botoes pagina perfil

---
+++ app/src/main/java/com/example/tsisocialapp/views/activities/PostListActivity.kt
@@ -0,0 +1,12 @@
+package com.example.tsisocialapp.views.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.tsisocialapp.R
+
+class PostListActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_post_list)
+    }
+}
--- app/src/main/java/com/example/tsisocialapp/views/fragments/ProfileFragment.kt
+++ app/src/main/java/com/example/tsisocialapp/views/fragments/ProfileFragment.kt
@@ -1,11 +1,14 @@
 package com.example.tsisocialapp.views.fragments
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.tsisocialapp.R
+import kotlinx.android.synthetic.main.fragment_profile.*
+import kotlinx.android.synthetic.main.options_card.view.*
 
 class ProfileFragment : Fragment() {
     override fun onCreateView(
@@ -15,6 +18,26 @@ class ProfileFragment : Fragment() {
         val view = inflater.inflate(R.layout.fragment_profile, container, false)
 
         return view
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        val posts = layoutInflater.inflate(R.layout.options_card, linearContainer, false)
+        posts.txtBtn.text = "Meus Posts"
+        linearContainer.addView(posts)
+
+        val favoritos = layoutInflater.inflate(R.layout.options_card, linearContainer, false)
+        favoritos.txtBtn.text = "Meus Favoritos"
+        linearContainer.addView(favoritos)
+
+        posts.setOnClickListener {
+           Log.e("Click posts", "Posts")
+        }
+
+        favoritos.setOnClickListener {
+            Log.e("Click favoritos", "Favoritos")
+        }
     }
 
     companion object {
